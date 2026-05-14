d7a1d3c72a07303867eb90e09dde5562ea920104
Guilherme Ravisio <guilherme.ravisio@gmail.com>
2026-05-14 08:07:12 -0300

    feat(API e config): Ajustes para utilizar front com vite.

--- Program.cs
+++ Program.cs
@@ -7,6 +7,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// Adicione a política de CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        policy => policy.WithOrigins("http://localhost:5173")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod());
+});
+
 builder.Services.AddScoped<IPatientService, PatientsService>(); 
 
 builder.Services.AddControllers();
@@ -19,9 +28,13 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// Use a política de CORS
+app.UseCors("AllowReactApp");
 
 app.UseHttpsRedirection();
 
@@ -30,6 +43,6 @@ app.UseAuthorization();
 app.MapControllers();
 
 // Mapeia a raiz para evitar 404 quando não houver uma rota padrão ou páginas estáticas
-app.MapGet("/", () => Results.Content("<html><body><h1>Aplicação em execução</h1><p>API disponível em <a href=\"/swagger\">Swagger</a> ou use as rotas dos controllers.</p></body></html>", "text/html"));
+app.MapGet("/", () => Results.Content("<html><body><h1>Aplicação em execução</h1><p>API disponível em <a href=\"/swagger\">Swagger</a> ou use as rotas dos controllers.</p></body></html>", "text/html; charset=utf-8"));
 
 app.Run();
